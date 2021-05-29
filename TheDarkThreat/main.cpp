0f40130101109d1ced3022c94d671669cd469506
Patryk Marczak <m.patryk@outlook.com>
2021-05-29 20:00:20 +0200

    Dodaj pliki projektów.

---
+++ TheDarkThreat/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <SFML\Graphics.hpp>
+#include <SFML\Window.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::Black);
+
+
+        window.display();
+    }
+
+    return 0;
+}
